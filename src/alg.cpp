98f83d749e317a8df5196deaeefbc994ef00cc5b
Sony Podrazska <146202175+Podrazik@users.noreply.github.com>
2025-04-06 11:28:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -25,52 +25,33 @@ int countPairs2(int *arr, int len, int value) {
   return count;
 }
 int binSearch(int *arr,int left, int right, int value) {
+  int pos = -1;
   while (left <= right) {
-      int mid = left + (right - left) / 2;
-      if (arr[mid] == value) return mid;
-      if (arr[mid] > value) right = mid - 1;
-      if (arr[mid] < value) left = mid + 1;
+    int mid = left + (right - left) / 2;
+    if (arr[mid] == value) {
+      pos = mid;
+      if (firstone) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else if (arr[mid] < value) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
   }
-  return -1;
+  return pos;
 }
 int countPairs3(int *arr, int len, int value) {
   int countAll = 0;
   for (int i = 0; i < len; i++) {
-    if (arr[i] > value / 2) continue;
-    if (i > 0 && arr[i] == arr[i - 1]) continue;
     int item = value - arr[i];
-    int ndx = binSearch(arr, i + 1, len - 1, item);
-    if (ndx != -1) {
-        if(arr[i] == item) {
-          int count = 1;
-          int j = i + 1;
-          while (j < len && arr[j] == arr[i]) {
-            count++;
-            j++;
-          }
-          int count_itm = 1;
-          j = ndx + 1;
-          while (j < len && arr[i] == item){
-            count_itm++;
-            j++;
-          } 
-          if (count > 1) countAll += count * (count - 1) / 2;                
-          } else if (arr[i] < item) {
-              int count_i = 1;
-              int j = i + 1;
-              while(j < len && arr[j] == arr[i]) {
-                count_i++;
-                j++;
-              }
-              int count_itm = 1;
-              j = ndx + 1;
-              while(j < len && arr[j] == item) {
-                count_itm++;
-                j++;
-              }
-              countAll += count_i * count_itm;
-          }
-      }
+    int firstItem = binSearch(arr, i + 1, len - 1, item, true); 
+    if (firstItem != -1) {
+        int lastItem = binSearch(arr, i + 1, len - 1, item, false);
+        countAll+= (lastItem - firstItem + 1);
+    }
   }
 return countAll;
 }
